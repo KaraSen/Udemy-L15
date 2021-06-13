fde25cfb33858341e5d7e3028a82277fea50965a
Sen <karasev01@yandex.ru>
2021-06-13 22:43:28 +0300

    15_while

--- JS/script.js
+++ JS/script.js
@@ -33,18 +33,37 @@ const UserMovieData = {
     else: false
 }
 
-for (let i = 0; i < 2; i++) {
+// for (let i = 0; i < 2; i++) {
 
-    const a = prompt("Один из последних просмотренных фильмов?", ""),  
-        b = +prompt("На сколько оцените его?", "");
+//     const a = prompt("Один из последних просмотренных фильмов?", ""),  
+//         b = +prompt("На сколько оцените его?", "");
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        UserMovieData.ViewedMoviesRate[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         UserMovieData.ViewedMoviesRate[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+// }
+let i = 0;
+
+while (i < 2) {
+    const a = prompt("Один из последних просмотренных фильмов?", ''),
+        b = prompt("На сколько оцените его?", "");
+        if ( a != null && b != null && a != '' && b != '' && a.length < 50) {
+            UserMovieData.ViewedMoviesRate[a] = b;
+            i++;
+            console.log('done');
+        } else {
+            i--;
+            console.log('error');
+            if (i<0){
+                i=0;
+            }
+        }
+
 }
+
 
 console.log(UserMovieData);
